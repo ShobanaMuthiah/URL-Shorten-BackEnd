08633e2d912d508c31a750818cd191ce2948f4c1
ShobanaMuthiah <shobanam222@gmail.com>
2024-06-13 12:49:10 -1200

    completed

--- Models/URL.js
+++ Models/URL.js
@@ -1,12 +1,14 @@
 import mongoose from 'mongoose';
 
-const URLSchema = new mongoose.Schema({
+const { Schema, model } = mongoose;
+
+const URLSchema = new Schema({
   originalUrl: { type: String, required: true },
   shortUrl: { type: String, required: true },
   clickCount: { type: Number, default: 0 },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  createdBy: { type: Number, ref: 'User' },
   createdAt: { type: Date, default: Date.now }
 });
 
- const URL= mongoose.model('URL', URLSchema);
- export default URL
+const URL = model('URL', URLSchema);
+export default URL;
